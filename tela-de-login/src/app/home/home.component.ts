fd5fb2e9ab1eaf1644c146be6632df2c6cb77c46
LeilaSousa <leilacb0@hotmail.com>
2022-10-20 16:40:19 -0300

    criei tela de login

---
+++ tela-de-login/src/app/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  msgLogin:string = 'Seja bem-vindo! Faça login para acessar a sua conta.'
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
